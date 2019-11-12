14c9a64f6872193a7d3423ed08c57f8b76e86ed6
Sempolus <kuli25@wp.pl>
2019-11-12 20:21:05 +0100

    dodanie nowej funkcjonalnosci

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,5 +10,6 @@ public class Main {
         System.out.println("Sprawdzamy wwersję 3");
         System.out.println("zmiana przez zewnetrznego usera");
         //polaczenie githuba i locala
+        System.out.println("nowa funkcjonalność");
     }
 }

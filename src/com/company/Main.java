456708603119a5c916455d94804f78f64d882566
Sempolus <kuli25@wp.pl>
2019-11-12 19:38:11 +0100

    start projektu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
-
+//CTRL + D duplikowaniw lini kodu
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        //sout --> skrot
+        System.out.println("Test projektu 1");
+        System.out.println("Test projektu 1");
     }
 }
